4975a396e3e6a7c0dbf30d2a716e9f64443597b5
erlend <erlenrom@stud.ntnu.no>
2024-04-10 10:17:01 +0200

    Added basic instantiation of server

--- cmd/api/server/server.go
+++ cmd/api/server/server.go
@@ -1,1 +1,15 @@
 package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"structure/internal/http/route"
+)
+
+func StartServer() {
+	router := route.NewRouter("8080")
+
+	log.Printf("Server started on port %s...\n", router.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", router.Port), router))
+}
